3e9b6daa4a6ebad070a07dd27134b6090fd2591e
Trevor McCasland <trevormccasland1@gmail.com>
2022-09-10 16:14:11 -0500

    add hero image to matches table

--- src/components/MatchTable.tsx
+++ src/components/MatchTable.tsx
@@ -10,55 +10,61 @@ interface MatchTableProps {
 const MatchTable: FC<MatchTableProps> = ({ match }): ReactElement => {
   return (
       <div>
-          <div className='headerContainer'>
-            <h2 className='Header'>Match ID: {match.match_id} Date: {new Date(match.start_time * 1000).toDateString()} Duration: {Math.round(match.duration / 60)} minutes {match.radiant_win ? 'Radiant Victory' : 'Dire Victory'}</h2>
-            <Link className='matchesLink' href={`/matches/${match.match_id}`}>Match Details</Link>
-          </div>
-          <table className="matchesTable">
-            <thead className='tableHeader'>
-              <tr>
-                <th>
-                  Hero Name
-                </th>
-                <th>
-                  Team Name
-                </th>
-                <th>
-                  Net Worth
-                </th>
-                <th>
-                  Kills
-                </th>
-                <th>
-                  Deaths
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-                {match.players.map((player, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>
-                        {player.hero_name.split('npc_dota_hero_')[1].replace('_', ' ')}
-                      </td>
-                      <td>
-                        {player.team_number === 1 ? 'Dire' : 'Radiant'}
-                      </td>
-                      <td>
-                        {player.net_worth}
-                      </td>
-                      <td>
-                        {player.kills}
-                      </td>
-                      <td>
-                        {player.deaths}
-                      </td>
-                    </tr>
-                  )
-                })}
-            </tbody>
-          </table>
+        <div className='headerContainer'>
+          <h2 className='Header'>Match ID: {match.match_id} Date: {new Date(match.start_time * 1000).toDateString()} Duration: {Math.round(match.duration / 60)} minutes {match.radiant_win ? 'Radiant Victory' : 'Dire Victory'}</h2>
+          <Link className='matchesLink' href={`/matches/${match.match_id}`}>Match Details</Link>
         </div>
+        <table className="matchesTable">
+          <thead className='tableHeader'>
+            <tr>
+              <th>
+                Photo
+              </th>
+              <th>
+                Hero Name
+              </th>
+              <th>
+                Team Name
+              </th>
+              <th>
+                Net Worth
+              </th>
+              <th>
+                Kills
+              </th>
+              <th>
+                Deaths
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+              {match.players.map((player, i) => {
+                return (
+                  <tr key={i}>
+                    <td>
+                      <img src={`http://cdn.dota2.com/apps/dota2/images/heroes/${player.hero_name.split('npc_dota_hero_')[1]}_sb.png`} />
+                    </td>
+                    <td>
+                      {player.hero_name.split('npc_dota_hero_')[1].replace('_', ' ')}
+                    </td>
+                    <td>
+                      {player.team_number === 1 ? 'Dire' : 'Radiant'}
+                    </td>
+                    <td>
+                      {player.net_worth}
+                    </td>
+                    <td>
+                      {player.kills}
+                    </td>
+                    <td>
+                      {player.deaths}
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+      </div>
   )
 }
 
