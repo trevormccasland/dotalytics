313a3d2c0ae906f005c69027f4733e9162b35e5f
Trevor McCasland <trevormccasland1@gmail.com>
2022-10-01 15:28:38 -0500

    get webpack running

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  mode: 'development',
   output: {
-    filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
+      template: path.join(__dirname, 'public', 'index.html')
+    })
   ],
   resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
   },
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: require.resolve("babel-loader"),
+        loader: require.resolve('babel-loader')
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.png|svg|jpg|gif$/,
-        use: ["file-loader"],
-      }, 
-    ],
-  },
-};
+        use: ['file-loader']
+      }
+    ]
+  }
+}
