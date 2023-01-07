be7b039086e9c1587fb71bd0b0efe574c0462f04
Trevor McCasland <trevormccasland1@gmail.com>
2023-01-07 10:21:41 -0600

    add icons to side bar

--- src/components/CategorySideBar.tsx
+++ src/components/CategorySideBar.tsx
@@ -1,20 +1,54 @@
 import React, { FC, ReactElement } from 'react'
 import './CategorySideBar.css'
+import { FaUserAlt, FaHistory, FaBan, FaChartLine, FaChartPie, FaCrosshairs } from 'react-icons/fa'
+
+interface Category {
+  name: string
+  icon: ReactElement
+}
 
 interface CategorySideBarProps {
   onCategoryChange: (index: number) => void
   selectedIndex: number
 }
-
+const categories: Category[] = [
+  {
+    name: 'Team Win %',
+    icon: <FaChartPie className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Kills',
+    icon: <FaCrosshairs className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Hero Picks',
+    icon: <FaUserAlt className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Ban Picks',
+    icon: <FaBan className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Item Win Rate',
+    icon: <FaChartLine className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Hero Win Rate',
+    icon: <FaChartLine className='sideBarIcon' size={30} />
+  },
+  {
+    name: 'Match History',
+    icon: <FaHistory className='sideBarIcon' size={30} />
+  }
+]
 const CategorySideBar: FC<CategorySideBarProps> = ({ onCategoryChange, selectedIndex }): ReactElement => {
   return <div className='sideBarContainer'>
-    <button className='sideBarButton' style={ selectedIndex === 0 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(0)}>Team Win %</button>
-    <button className='sideBarButton' style={ selectedIndex === 1 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(1)}>Kills</button>
-    <button className='sideBarButton' style={ selectedIndex === 2 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(2)}>Hero Picks</button>
-    <button className='sideBarButton' style={ selectedIndex === 3 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(3)}>Ban Picks</button>
-    <button className='sideBarButton' style={ selectedIndex === 4 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(4)}>Item Win Rate</button>
-    <button className='sideBarButton' style={ selectedIndex === 5 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(5)}>Hero Win Rate</button>
-    <button className='sideBarButton' style={ selectedIndex === 6 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(6)}>Match History</button>
+    {categories.map((category, i) => (
+      <div key={i} className='sideBarItem' style={ selectedIndex === i ? { backgroundColor: 'gainsboro' } : {}}>
+        <button className='sideBarButton' onClick={() => onCategoryChange(i)}>{category.name}</button>
+        {category.icon}
+      </div>
+    ))}
   </div>
 }
 
