64380f36b000e1af9f5afbaef720247f9c1c4dba
Trevor McCasland <trevormccasland1@gmail.com>
2023-01-07 11:17:55 -0600

    Add icons (#40)
    * add icons to side bar
    
    * make sidebar collapsible

--- src/components/CategorySideBar.tsx
+++ src/components/CategorySideBar.tsx
@@ -1,5 +1,11 @@
-import React, { FC, ReactElement } from 'react'
+import React, { FC, ReactElement, useMemo, useState } from 'react'
 import './CategorySideBar.css'
+import { FaArrowRight, FaArrowLeft, FaUserAlt, FaHistory, FaBan, FaChartLine, FaChartPie, FaCrosshairs } from 'react-icons/fa'
+
+interface Category {
+  name: string
+  icon: ReactElement
+}
 
 interface CategorySideBarProps {
   onCategoryChange: (index: number) => void
@@ -7,14 +13,52 @@ interface CategorySideBarProps {
 }
 
 const CategorySideBar: FC<CategorySideBarProps> = ({ onCategoryChange, selectedIndex }): ReactElement => {
-  return <div className='sideBarContainer'>
-    <button className='sideBarButton' style={ selectedIndex === 0 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(0)}>Team Win %</button>
-    <button className='sideBarButton' style={ selectedIndex === 1 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(1)}>Kills</button>
-    <button className='sideBarButton' style={ selectedIndex === 2 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(2)}>Hero Picks</button>
-    <button className='sideBarButton' style={ selectedIndex === 3 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(3)}>Ban Picks</button>
-    <button className='sideBarButton' style={ selectedIndex === 4 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(4)}>Item Win Rate</button>
-    <button className='sideBarButton' style={ selectedIndex === 5 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(5)}>Hero Win Rate</button>
-    <button className='sideBarButton' style={ selectedIndex === 6 ? { backgroundColor: 'gainsboro' } : {}} onClick={() => onCategoryChange(6)}>Match History</button>
+  const [open, setOpen] = useState(true)
+  const categories: Category[] = useMemo(() => {
+    const className = open ? 'sideBarIcon' : 'sideBarIconCollapsed'
+    return [
+      {
+        name: 'Team Win %',
+        icon: <FaChartPie className={className} size={30} />
+      },
+      {
+        name: 'Kills',
+        icon: <FaCrosshairs className={className} size={30} />
+      },
+      {
+        name: 'Hero Picks',
+        icon: <FaUserAlt className={className} size={30} />
+      },
+      {
+        name: 'Ban Picks',
+        icon: <FaBan className={className} size={30} />
+      },
+      {
+        name: 'Item Win Rate',
+        icon: <FaChartLine className={className} size={30} />
+      },
+      {
+        name: 'Hero Win Rate',
+        icon: <FaChartLine className={className} size={30} />
+      },
+      {
+        name: 'Match History',
+        icon: <FaHistory className={className} size={30} />
+      }
+    ]
+  }, [open])
+  return <div>
+    <div className={open ? 'sideBarContainer' : 'sideBarContainerCollapsed'}>
+      {categories.map((category, i) => (
+        <div key={i} className='sideBarItem' onClick={() => onCategoryChange(i)} style={ selectedIndex === i ? { backgroundColor: 'gainsboro' } : {}}>
+          {open && <p className='sideBarText'>{category.name}</p>}
+          {category.icon}
+        </div>
+      ))}
+      <div className='sideBarItem' onClick={() => setOpen((prev) => !prev)}>
+        {open ? <FaArrowLeft className='sideBarLeftArrow' size={30} /> : <FaArrowRight className='sideBarRightArrow' size={30} />}
+      </div>
+    </div>
   </div>
 }
 
